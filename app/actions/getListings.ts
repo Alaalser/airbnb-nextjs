354da4909ada1a2d92893d275fbd447239d9a055
Alaa Alser <alaalsser@gmail.com>
2023-04-05 18:02:18 +0300

    create reservation and faviort logic

---
+++ app/actions/getListings.ts
@@ -0,0 +1,21 @@
+import prisma from "../libs/prismadb";
+
+export default async function getListings() {
+  try {
+    const listings = await prisma.listing.findMany({
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+    const safeListings = listings.map((listing) => {
+      return {
+        ...listing,
+        createdAt: listing.createdAt.toISOString(),
+      };
+    });
+
+    return safeListings;
+  } catch (error: any) {
+    throw new Error(error);
+  }
+}
